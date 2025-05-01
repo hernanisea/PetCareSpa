9bfa04563eb5e8afd454ce96d4d463ecc2c430ec
hernanisea <he.isea@duocuc.cl>
2025-05-01 17:21:07 -0400

    Add files via upload

---
+++ HistorialClinico/src/test/java/com/Microservicios/HistorialClinico/HistorialClinicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservicios.HistorialClinico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorialClinicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
