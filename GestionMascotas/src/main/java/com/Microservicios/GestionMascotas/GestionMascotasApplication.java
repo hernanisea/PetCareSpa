822465623c92727000931d955d48160791bbe048
hernanisea <he.isea@duocuc.cl>
2025-05-01 17:18:12 -0400

    Add files via upload

---
+++ GestionMascotas/src/main/java/com/Microservicios/GestionMascotas/GestionMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.Microservicios.GestionMascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMascotasApplication.class, args);
+	}
+
+}
---
+++ GestionMascotas/src/test/java/com/Microservicios/GestionMascotas/GestionMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservicios.GestionMascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
