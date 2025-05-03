ed5c42780897905bc4eb6657308ca92699358a08
Orangeboii <nico.cuevasl@duocuc.cl>
2025-05-02 20:39:08 -0400

    Add files via upload

---
+++ ReseñasyCalificaciones/ReseñasyCalificaciones/src/test/java/com/Microservicios/ResenasyCalificaciones/ResenasyCalificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservicios.ResenasyCalificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenasyCalificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
