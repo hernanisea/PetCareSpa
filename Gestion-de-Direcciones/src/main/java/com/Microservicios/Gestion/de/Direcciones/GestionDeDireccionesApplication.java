07c5f3415c5d9636aa1815a697891170de7ece00
hernanisea <he.isea@duocuc.cl>
2025-05-10 16:13:30 -0400

    Add files via upload

---
+++ Gestion-de-Direcciones/src/main/java/com/Microservicios/Gestion/de/Direcciones/GestionDeDireccionesApplication.java
@@ -0,0 +1,13 @@
+package com.Microservicios.Gestion.de.Direcciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeDireccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeDireccionesApplication.class, args);
+	}
+
+}
---
+++ Gestion-de-Direcciones/src/test/java/com/Microservicios/Gestion/de/Direcciones/GestionDeDireccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservicios.Gestion.de.Direcciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeDireccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
