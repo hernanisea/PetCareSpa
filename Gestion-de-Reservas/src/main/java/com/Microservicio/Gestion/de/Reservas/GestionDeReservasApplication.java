b9cfbac46fd7e61f55335798b2b623d1e62101cb
hernanisea <he.isea@duocuc.cl>
2025-05-14 13:09:32 -0400

    Add files via upload

---
+++ Gestion-de-Reservas/src/main/java/com/Microservicio/Gestion/de/Reservas/GestionDeReservasApplication.java
@@ -0,0 +1,13 @@
+package com.Microservicio.Gestion.de.Reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeReservasApplication.class, args);
+	}
+
+}
