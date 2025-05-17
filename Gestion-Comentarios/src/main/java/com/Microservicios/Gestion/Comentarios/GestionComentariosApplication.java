c304c62c4e351e4623794d6eb126a9c0c3ab46cc
hernanisea <he.isea@duocuc.cl>
2025-05-16 21:29:21 -0400

    Add files via upload

---
+++ Gestion-Comentarios/src/main/java/com/Microservicios/Gestion/Comentarios/GestionComentariosApplication.java
@@ -0,0 +1,13 @@
+package com.Microservicios.Gestion.Comentarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionComentariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionComentariosApplication.class, args);
+	}
+
+}
---
+++ Gestion-Comentarios/src/test/java/com/Microservicios/Gestion/Comentarios/GestionComentariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservicios.Gestion.Comentarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionComentariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
