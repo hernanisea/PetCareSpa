27b939c65b58913a8878bca0b1acf2bf8debc147
Orangeboii <nico.cuevasl@duocuc.cl>
2025-06-14 13:13:37 -0400

    Add files via upload

---
+++ GestionDeServicios/GestionDeServicios/src/test/java/com/example/GestionDeServicios/GestionDeServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionDeServicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
