7c2410f113346c1ad64a208c25a9380ebb77cf94
hernanisea <he.isea@duocuc.cl>
2025-06-23 21:13:32 -0400

    gestion de usuarios
    se crearon los testing

--- GestionUsuarios/src/main/java/com/Microservicios/GestionUsuarios/repository/UsuarioRepository.java
+++ GestionUsuarios/src/main/java/com/Microservicios/GestionUsuarios/repository/UsuarioRepository.java
@@ -1,11 +1,18 @@
 package com.Microservicios.GestionUsuarios.repository;
 
-import com.Microservicios.GestionUsuarios.model.Usuario;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.Microservicios.GestionUsuarios.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    // ✅ Verifica si existe un usuario por correo (booleano)
     boolean existsByCorreo(String correo);
+
+    // ✅ Devuelve un Optional<Usuario> por correo
+    Usuario findByCorreo(String correo);
 }
